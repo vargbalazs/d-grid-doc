ec50f09b9e0e9ebc635f4a4db373ef1bdc60290b
vargbalazs <balazs.varga.8564@gmail.com>
2025-09-15 19:43:21 +0200

    adding row templates

--- src/app/docs/core-features/rows/templates/templates.ts
+++ src/app/docs/core-features/rows/templates/templates.ts
@@ -7,4 +7,30 @@ import { CodeBlockComponent } from '../../../../components/code-block/code-block
   templateUrl: './templates.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RowTemplatesComponent {}
+export class RowTemplatesComponent {
+  loadingTempl = `
+    <d-grid [data]="gridData()" [asyncData]="true">
+      <d-grid-column field="id" header="id" [width]="50"> </d-grid-column>
+      <d-grid-column field="id" header="id2" [width]="50"></d-grid-column>
+      <d-grid-column field="name" header="name" [width]="100"></d-grid-column>
+      <d-grid-column field="email" header="email" [width]="150"> </d-grid-column>
+      <d-grid-column field="active" header="active" [width]="100"> </d-grid-column>
+      <ng-template dGridLoadingTemplate>
+        <div>custom loading template</div>
+      </ng-template>
+    </d-grid>
+    `;
+
+  noDataTempl = `
+    <d-grid [data]="gridData()" [asyncData]="true">
+      <d-grid-column field="id" header="id" [width]="50"> </d-grid-column>
+      <d-grid-column field="id" header="id2" [width]="50"></d-grid-column>
+      <d-grid-column field="name" header="name" [width]="100"></d-grid-column>
+      <d-grid-column field="email" header="email" [width]="150"> </d-grid-column>
+      <d-grid-column field="active" header="active" [width]="100"> </d-grid-column>
+      <ng-template dGridNoDataTemplate>
+        <div>custom no-data template</div>
+      </ng-template>
+    </d-grid>
+  `;
+}
