e12875627cec35db811baa119cb58187157af108
vargbalazs <balazs.varga.8564@gmail.com>
2025-09-16 14:49:25 +0200

    remove codeblock comp

--- src/app/docs/layout-styling/variables/variables.ts
+++ src/app/docs/layout-styling/variables/variables.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CodeBlockComponent } from '../../../components/code-block/code-block';
 import { RouterLink } from '@angular/router';
 import { INTERNAL_VARIABLES } from './variables-data';
 
 @Component({
   selector: 'docs-layout-styling-variables',
-  imports: [CodeBlockComponent, RouterLink],
+  imports: [RouterLink],
   templateUrl: './variables.html',
   styleUrl: './variables.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
