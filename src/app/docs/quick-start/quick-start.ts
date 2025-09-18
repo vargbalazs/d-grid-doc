c538fc8c60981bf70334fab844ed7d81ff7cc978
vargbalazs <balazs.varga.8564@gmail.com>
2025-09-18 19:43:30 +0200

    working on quick start

--- src/app/docs/quick-start/quick-start.ts
+++ src/app/docs/quick-start/quick-start.ts
@@ -6,6 +6,13 @@ import { RouterLink } from '@angular/router';
   selector: 'docs-quick-start',
   imports: [CodeBlockComponent, RouterLink],
   templateUrl: './quick-start.html',
+  styleUrl: './quick-start.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuickStartComponent {}
+export class QuickStartComponent {
+  npm = `
+    
+        npm i d-grid-angular
+    
+    `;
+}
