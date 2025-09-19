fa993dd3c034c7173e159eb088defbf09050f9f6
vargbalazs <balazs.varga.8564@gmail.com>
2025-09-19 11:52:26 +0200

    working on quick start example 2

--- src/app/docs/quick-start/example/example.ts
+++ src/app/docs/quick-start/example/example.ts
@@ -1,11 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CodeBlockComponent } from '../../../components/code-block/code-block';
-import { RouterLink } from '@angular/router';
+import { D_GRID } from 'd-grid-angular';
+import { Row } from './row';
+import { sampleData } from './data';
 
 @Component({
   selector: 'docs-quick-start-example',
-  imports: [CodeBlockComponent, RouterLink],
+  imports: [CodeBlockComponent, D_GRID],
   templateUrl: './example.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuickStartExampleComponent {}
+export class QuickStartExampleComponent {
+  gridData = signal<Row[]>(sampleData);
+}
--- src/app/docs/quick-start/quick-start.ts
+++ src/app/docs/quick-start/quick-start.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CodeBlockComponent } from '../../components/code-block/code-block';
 import { RouterLink } from '@angular/router';
+import { QuickStartExampleComponent } from './example/example';
 
 @Component({
   selector: 'docs-quick-start',
-  imports: [CodeBlockComponent, RouterLink],
+  imports: [CodeBlockComponent, RouterLink, QuickStartExampleComponent],
   templateUrl: './quick-start.html',
   styleUrl: './quick-start.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
