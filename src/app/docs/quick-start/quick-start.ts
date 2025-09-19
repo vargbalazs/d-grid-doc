ac294e4d9f1d3d8a098ff19853890d7541965627
vargbalazs <balazs.varga.8564@gmail.com>
2025-09-19 12:24:54 +0200

    quick start example 3

--- src/app/docs/quick-start/quick-start.ts
+++ src/app/docs/quick-start/quick-start.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CodeBlockComponent } from '../../components/code-block/code-block';
 import { RouterLink } from '@angular/router';
 import { QuickStartExampleComponent } from './example/example';
+import { KENDO_TABSTRIP } from '@progress/kendo-angular-layout';
 
 @Component({
   selector: 'docs-quick-start',
-  imports: [CodeBlockComponent, RouterLink, QuickStartExampleComponent],
+  imports: [CodeBlockComponent, RouterLink, QuickStartExampleComponent, KENDO_TABSTRIP],
   templateUrl: './quick-start.html',
   styleUrl: './quick-start.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
