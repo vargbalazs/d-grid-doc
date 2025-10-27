afad5f597bd0d7ee29e01253ce8dd1c2504bdd88
vargbalazs <balazs.varga.8564@gmail.com>
2025-10-27 19:06:05 +0100

    completing filtering row example

---
+++ src/app/docs/core-features/filtering/row/example/data.ts
@@ -0,0 +1,30 @@
+import { Row } from './row';
+
+export const sampleData: Row[] = [
+  { id: 1, name: 'Hinze', email: 'hchamberlaine0@foxnews.com', active: false, date: '2025-10-27' },
+  { id: 2, name: 'Cayla', email: 'csmyley1@people.com.cn', active: true, date: '2025-10-27' },
+  { id: 3, name: 'Haslett', email: 'hbaldery2@myspace.com', active: true, date: '2025-10-27' },
+  { id: 4, name: 'Yvonne', email: 'ypaggitt3@xrea.com', active: true, date: '2025-10-27' },
+  { id: 5, name: 'Diana', email: 'dlusher4@weather.com', active: true, date: '2025-10-27' },
+  { id: 6, name: 'Leesa', email: 'lgilbank5@tinyurl.com', active: true, date: '2025-10-27' },
+  { id: 7, name: 'Pattie', email: 'pwoodhall6@zdnet.com', active: true, date: '2025-10-27' },
+  { id: 8, name: 'Wye', email: 'wkupker7@wisc.edu', active: false, date: '2025-10-27' },
+  { id: 9, name: 'Selia', email: 'sheathcote8@oaic.gov.au', active: false, date: '2025-10-27' },
+  { id: 10, name: 'Lolly', email: 'lpagram9@behance.net', active: true, date: '2025-10-27' },
+  { id: 11, name: 'Elmer', email: 'esudlowa@vk.com', active: false, date: '2025-10-27' },
+  {
+    id: 12,
+    name: 'Rozalie',
+    email: 'rmclarenb@chronoengine.com',
+    active: true,
+    date: '2025-10-27',
+  },
+  { id: 13, name: 'Amory', email: 'adurnallc@dion.ne.jp', active: false, date: '2023-10-27' },
+  { id: 14, name: 'Willow', email: 'wbonnickd@yolasite.com', active: false, date: '2025-10-27' },
+  { id: 15, name: 'Jone', email: 'jdurnforde@istockphoto.com', active: true, date: '2025-10-27' },
+  { id: 16, name: 'Ethelyn', email: 'eferrelif@google.es', active: true, date: '2024-10-27' },
+  { id: 17, name: 'Errick', email: 'ebrandsg@statcounter.com', active: true, date: '2024-10-27' },
+  { id: 18, name: 'Diego', email: 'dlotteh@newyorker.com', active: false, date: '2025-08-27' },
+  { id: 19, name: 'Becky', email: 'blisamorei@sbwire.com', active: false, date: '2025-09-27' },
+  { id: 20, name: 'Lind', email: 'lcluelowj@stumbleupon.com', active: true, date: '2025-10-25' },
+];
